af2c76885b21605cc5fdad44f01d1b59767e3e3e
ted737 <angelazhang1549@gmail.com>
2022-01-02 18:28:55 -0500

    save

--- src/App.js
+++ src/App.js
@@ -20,14 +20,26 @@ import Faq from "./pages/Faq/Faq";
 export default function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="ted737">
-        {/* <BrowserRouter> */}
+      {/* <BrowserRouter basename="ted737"> */}
+      {/* <BrowserRouter> */}
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
-          <Route path="/" element={<Landing />} />
+          {/* <Route path="/" element={<Landing />} />
           <Route path="/About" element={<About />} />
           <Route path="/Partners" element={<Partners />} />
-          <Route path="/Speakers" element={<Speakers />} />
-          <Route path="/Faq" element={<Faq />} />
+          <Route path="/Speakers" element={<Speakers />} /> */}
+          {/* <Route path="/Faq" element={<Faq />} /> */}
+          <Route path={process.env.PUBLIC_URL + "/"} element={<Landing />} />
+          <Route path={process.env.PUBLIC_URL + "/About"} element={<About />} />
+          <Route
+            path={process.env.PUBLIC_URL + "/Partners"}
+            element={<Partners />}
+          />
+          <Route
+            path={process.env.PUBLIC_URL + "/Speakers"}
+            element={<Speakers />}
+          />
+          <Route path={process.env.PUBLIC_URL + "/Faq"} element={<Faq />} />
         </Routes>
       </BrowserRouter>
       <Footer />
